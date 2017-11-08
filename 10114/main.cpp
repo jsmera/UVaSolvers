7a990d86d479b21dff6822185bded0b5a7dab343
Carlos <merac1999@gmail.com>
2017-11-08 10:44:51 -0500

    Aceptado 10114

--- 10114/main.cpp
+++ 10114/main.cpp
@@ -5,38 +5,38 @@ using namespace std;
 int main() {
   while (true) {
     int moths, n_res;
-    double first, prestamo;
-    scanf("%d %lf %lf %d", &moths, &first, &prestamo, &n_res);
+    double first, prestamo, rats[101];
+    scanf("%d %lf %lf %d\n", &moths, &first, &prestamo, &n_res);
     if (moths < 0)
       break;
-    double rats[101];
-    double devaluo = first + prestamo;
-    
-    for (int i = 0; i < 101; i++) {
+
+    for (int i = 0; i <= moths; i++) {
       rats[i] = 0;
     }
 
     while (n_res--) {
       int n_moth;
       double rat;
-      scanf("%d %lf", &n_moth, &rat);
+      scanf("%d %lf\n", &n_moth, &rat);
       rats[n_moth] = rat;
     }
-    devaluo-=devaluo*rats[0];
-    prestamo-=first;
 
-    int res = 1;
-    double before_rat = rats[0];
-    /*while (devaluo < prestamo) {
-      before_rat = rats[res] == 0? before_rat: rats[res];
-      devaluo -= devaluo*before_rat;
-      prestamo -= first;
+    double before_rat = rats[0], pago = prestamo/moths;
+    double temp = prestamo, devaluo = prestamo + first;
+    devaluo -= devaluo*rats[0];
+    int res = 0;
+
+    while (devaluo < temp) {
       res++;
-    }*/
-    for (int i = 0; i < 101; i++) {
-      printf("%d %lf\n", i, rats[res]);
+      if (rats[res] != 0)
+        before_rat = rats[res];
+      devaluo -= devaluo*before_rat;
+      temp -= pago;
     }
-    printf("%lf\n", before_rat);
+    if (res > 1 || res == 0)
+      printf("%d months\n", res);
+    else
+      printf("%d month\n", res);
   }
   return 0;
 }
